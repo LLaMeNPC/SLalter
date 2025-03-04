2c7288ab5e44da49fd0d15b8171d8dafb1e530bd
habr <habr@itu.dk>
2025-03-04 14:39:58 +0100

    added batch alterations

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-import os
+import os, json, datetime, sys, math
 from rewriter import Rewriter
 
 
@@ -16,6 +16,20 @@ modes = [
 
 def cls():
     os.system('cls' if os.name=='nt' else 'clear')
+
+
+def delete_last_line():
+    "Deletes the last line in the STDOUT"
+    # cursor up one line
+    sys.stdout.write('\x1b[1A')
+    # delete last line
+    sys.stdout.write('\x1b[2K')
+
+def print_progress(progress : float):
+    #    if progress < 0 or progress > 1:
+    #    raise Exception("Invalid progress given")
+    progress_string = "█" * math.floor(progress * 50) + "-" * math.ceil((1-progress) * 50) + f"| {progress * 100:.2f}%"
+    print(progress_string)
 
 def choice(ls):
     for i, le in enumerate(ls):
@@ -67,3 +81,33 @@ while True:
             else:
                 print(f"\"{_user_input}\" not recognized")
             _user_input = input("Input \"new\" to write a new sentence, \"again\" to regenerate from the same sentence, or \"continue\" to regenerate from the previous output: ")
+    elif selected_mode == modes[1]:
+        cls()
+        while True:
+            print("Which batch would you like to generate alterations on?")
+            batches = os.listdir("batches")
+            batch_name = get_element_from_choice(choice(batches), batches)
+            if batch_name != None:
+                cls()
+                num_alterations = int(input("How many alterations would you like to generate per sentence? "))
+                with open(f"batches/{batch_name}") as batch_data:
+                    batch_json = json.load(batch_data)
+                    output_filename = f"{batch_name}-alteration-{datetime.datetime.now()}"
+                    output = {}
+                    print("Rewriting sentences...")
+                    progress = 0.0
+                    print_progress(progress)
+                    for sentence in batch_json:
+                        output[sentence] = []
+                        for _ in range(num_alterations):
+                            output[sentence].append(rewriter.rewrite(sentence))
+                            progress += 1.0 / (len(batch_json) * num_alterations)
+                            delete_last_line()
+                            print_progress(progress)
+                        with open(f"output/{output_filename}.json", "w") as f:
+                            f.write(json.dumps(output))
+                    print(f"Done! Wrote output to output/{output_filename}.json")
+            else:
+                print("Batch not recognized")
+
+
