b7afad227fcf90dac8c3cc299eb0d622e7b75337
Daniel Fich <danielfich2@gmail.com>
2025-03-06 12:52:24 +0100

    Log moved into log.py

---
+++ src/log.py
@@ -0,0 +1,13 @@
+import datetime
+
+log_file_path = "logs/{}.txt"
+
+def log(command, output=None):
+    date = datetime.datetime.now().date()
+    time = datetime.datetime.now().time().strftime('%H:%M:%S')
+
+    current_log_file_path = log_file_path.format(date)
+    with open(current_log_file_path, 'a') as file:
+        file.write(f"{time} {command}\n\n")
+        if output is not None:
+            file.write(f"\t{output}\n\n")
--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 import os, json, datetime, sys, math
 from rewriter import Rewriter
-
-log_file_path = "logs/{}.txt"
+from log import log
 
 models = [
     "smollm2:135m",
@@ -50,16 +49,6 @@ def generate(sentence, rewriter):
     output = rewriter.rewrite(sentence)
     print(f"\n{output}\n")
     return output
-
-def log(command, output=None):
-    date = datetime.datetime.now().date()
-    time = datetime.datetime.now().time().strftime('%H:%M:%S')
-
-    current_log_file_path = log_file_path.format(date)
-    with open(current_log_file_path, 'a') as file:
-        file.write(f"{time} {command}\n\n")
-        if output is not None:
-            file.write(f"\t{output}\n\n")
 
 selected_model = None
 selected_mode = None
